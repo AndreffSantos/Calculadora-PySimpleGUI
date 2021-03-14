ac8acc9483fd2d01a83cadfbf83c0577145f3dce
Andre Santos <andrefelipe_santos@outlook.com>
2021-03-14 19:13:48 -0300

    Implementando todas as operações.

--- Calculadora.py
+++ Calculadora.py
@@ -1,40 +1,56 @@
 import PySimpleGUI as psg
 
-class Interface:
+class App:
     def __init__(self):
         # Layout.
         layout = [
             [psg.Text('Calculadora')],
-            [psg.Text('', key='mostrador', size = (10, 1), text_color="White", font=('Helvetica', 18))],
-            [psg.Button('7'), psg.Button('8'), psg.Button('9')],
-            [psg.Button('4'), psg.Button('5'), psg.Button('6')],
-            [psg.Button('1'), psg.Button('2'), psg.Button('3')],
-            [psg.Button('+'), psg.Button('0'), psg.Button('=')],
+            [psg.Input('0', key='display', size = (15, 1), text_color="Black", font=('Helvetica', 20))],
+            [psg.Button('7'), psg.Button('8'), psg.Button('9'), psg.Button('C')],
+            [psg.Button('4'), psg.Button('5'), psg.Button('6'), psg.Button('/')],
+            [psg.Button('1'), psg.Button('2'), psg.Button('3'), psg.Button('*')],
+            [psg.Button('0'), psg.Button('-'), psg.Button('+'), psg.Button('=')],
         ]
+        self.result = 0
+        self.oper = ''
+        self.equacao = ''
 
         # Janela.
-        self.janela = psg.Window('Calculadora', layout, auto_size_buttons=False, default_button_element_size=(5,2),)
-    
-    def calcular(self):
-        elementos = 0
-        equacao = ''
+        self.janela = psg.Window('Calculadora', layout=layout, margins=(0,0), auto_size_buttons=False, return_keyboard_events=False, default_button_element_size=(5,2))
+                     
+    def iniciar(self):
         while True:
-            self.event, self.values = self.janela.Read()
-            if(self.event == psg.WIN_CLOSED):
-                print
+            self.event, self.values = self.janela.read()
+
+            if self.event == psg.WIN_CLOSED:
                 break
+                
+            elif self.event in ('0123456789'):
+                self.equacao += self.event
+                self.janela['display'].update(self.equacao)
 
-            elif(self.event in '0123456789'):
-                elementos = elementos + int(self.event)
-                equacao = equacao + self.event
-                self.janela['mostrador'].update(equacao)
-
-            elif(self.event == '+'):
-                equacao = equacao + self.event
-                self.janela['mostrador'].update(equacao)
-
-            elif(self.event == '='):
-                self.janela['mostrador'].update(elementos)
+            elif self.event in ('+-*/'):
+                self.oper = self.event
+                self.result = self.values['display']
+                self.equacao += self.event
+                self.janela['display'].update(self.equacao)
             
-calc = Interface()
-calc.calcular()
+            elif self.event in ('='):
+                self.result = self.operacao()
+                self.equacao = str(self.result)
+                self.janela['display'].update(self.result)
+                self.result = 0
+                self.oper = ''
+
+    def operacao(self):
+        if self.oper == '+':
+            return float(self.result) + float(self.values['display'][self.values['display'].find('+') + 1])
+        if self.oper == '-':
+            return float(self.result) - float(self.values['display'][self.values['display'].find('-') + 1])
+        if self.oper == '*':
+            return float(self.result) * float(self.values['display'][self.values['display'].find('*') + 1])
+        if self.oper == '/':
+            return float(self.result) / float(self.values['display'][self.values['display'].find('/') + 1])
+
+calculadora = App()
+calculadora.iniciar()
